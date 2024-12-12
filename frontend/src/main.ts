3247a397405a84ee93ded10810ec8c6bcf228567
Willem van der Schyff <willemvds@gmail.com>
2024-12-12 13:03:22 +0200

    style: Run ts-standard --fix.

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,35 +1,33 @@
 import { Document } from './webapi.ts'
 
-const handlePaste = function(ev: ClipboardEvent, mainDiv: Element) {
-  ev.preventDefault();
-  console.debug("paste event = ", ev);
-  if (ev.clipboardData) {
-    let pastedText = ev.clipboardData.getData("text");
-    let nte = document.createElement("p");
-    nte.appendChild(document.createTextNode(pastedText));
-    mainDiv.appendChild(nte);
+const handlePaste = function (ev: ClipboardEvent, mainDiv: Element) {
+  ev.preventDefault()
+  console.debug('paste event = ', ev)
+  if (ev.clipboardData != null) {
+    const pastedText = ev.clipboardData.getData('text')
+    const nte = document.createElement('p')
+    nte.appendChild(document.createTextNode(pastedText))
+    mainDiv.appendChild(nte)
   }
 }
 
-function initMain() {
-  const mainDiv = document.querySelector("#main");
+function initMain () {
+  const mainDiv = document.querySelector('#main')
 
-  if (!mainDiv) {
-    console.log("init failed");
-    return;
+  if (mainDiv == null) {
+    console.log('init failed')
+    return
   }
 
-  document.addEventListener('paste', function(ev) {
-	  handlePaste(ev, mainDiv);
-  });
-
+  document.addEventListener('paste', function (ev) {
+	  handlePaste(ev, mainDiv)
+  })
 }
 
 if (document.readyState === Document.STATE_LOADING) {
   // Loading hasn't finished yet
-  document.addEventListener(Document.EVENT_DOM_CONTENT_LOADED, initMain);
+  document.addEventListener(Document.EVENT_DOM_CONTENT_LOADED, initMain)
 } else {
   // `DOMContentLoaded` has already fired
-  initMain();
+  initMain()
 }
-
--- frontend/src/webapi.ts
+++ frontend/src/webapi.ts
@@ -1,5 +1,5 @@
 export namespace Document {
-  export const STATE_LOADING = "loading";
+  export const STATE_LOADING = 'loading'
 
-  export const EVENT_DOM_CONTENT_LOADED = "DOMContentLoaded";
+  export const EVENT_DOM_CONTENT_LOADED = 'DOMContentLoaded'
 }
