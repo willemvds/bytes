2a2d0a7454db6bdf994fc5945d48bf3d2d8574dd
Willem van der Schyff <willemvds@gmail.com>
2024-12-17 13:43:35 +0200

    feat: Begin some ground work to support uploading.

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -7,6 +7,7 @@ import {
 } from './webapi.ts'
 
 const mainDivId = 'main'
+const uploadButtonId = 'upload'
 
 const imageExtensions = ['bmp', 'gif', 'jpeg', 'jpg', 'png', 'tif', 'tiff', 'webp']
 
@@ -26,21 +27,84 @@ function handleDragOver (ev: Event, mainDiv: Element) {
   console.debug(mainDiv)
 }
 
-function showImage (mainDiv: Element, content: string) {
+// https://gist.github.com/jonleighton/958841
+function arrayBufferToBase64(arrayBuffer) {
+  var base64    = ''
+  var encodings = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+
+  var bytes         = new Uint8Array(arrayBuffer)
+  var byteLength    = bytes.byteLength
+  var byteRemainder = byteLength % 3
+  var mainLength    = byteLength - byteRemainder
+
+  var a, b, c, d
+  var chunk
+
+  // Main loop deals with bytes in chunks of 3
+  for (var i = 0; i < mainLength; i = i + 3) {
+    // Combine the three bytes into a single integer
+    chunk = (bytes[i] << 16) | (bytes[i + 1] << 8) | bytes[i + 2]
+
+    // Use bitmasks to extract 6-bit segments from the triplet
+    a = (chunk & 16515072) >> 18 // 16515072 = (2^6 - 1) << 18
+    b = (chunk & 258048)   >> 12 // 258048   = (2^6 - 1) << 12
+    c = (chunk & 4032)     >>  6 // 4032     = (2^6 - 1) << 6
+    d = chunk & 63               // 63       = 2^6 - 1
+
+    // Convert the raw binary segments to the appropriate ASCII encoding
+    base64 += encodings[a] + encodings[b] + encodings[c] + encodings[d]
+  }
+
+  // Deal with the remaining bytes and padding
+  if (byteRemainder == 1) {
+    chunk = bytes[mainLength]
+
+    a = (chunk & 252) >> 2 // 252 = (2^6 - 1) << 2
+
+    // Set the 4 least significant bits to zero
+    b = (chunk & 3)   << 4 // 3   = 2^2 - 1
+
+    base64 += encodings[a] + encodings[b] + '=='
+  } else if (byteRemainder == 2) {
+    chunk = (bytes[mainLength] << 8) | bytes[mainLength + 1]
+
+    a = (chunk & 64512) >> 10 // 64512 = (2^6 - 1) << 10
+    b = (chunk & 1008)  >>  4 // 1008  = (2^6 - 1) << 4
+
+    // Set the 2 least significant bits to zero
+    c = (chunk & 15)    <<  2 // 15    = 2^4 - 1
+
+    base64 += encodings[a] + encodings[b] + encodings[c] + '='
+  }
+  
+  return base64
+}
+
+function showImage (mainDiv: Element, content: ArrayBuffer) {
+  const imageBytes = new Uint8Array(content)
+//  const imageBase64 = imageBytes.toBase64()
+//  const dec = new TextDecoder('ascii');
+  const imageBase64 = arrayBufferToBase64(imageBytes)
+  console.debug("base64 = ", imageBase64)
+  const imageDataURL = 'data:image/jpg;base64,' + imageBase64
   const nte = document.createElement('img')
-  nte.src = content
-  nte.className = 'user_image'
+  nte.src = imageDataURL
+  nte.className = 'imageview'
   // nte.appendChild(document.createTextNode(contents))
   mainDiv.appendChild(nte)
 }
 
-function showText(mainDiv: Element, content: string) {
+function showText(mainDiv: Element, content: ArrayBuffer) {
+  const dec = new TextDecoder();
   const nte = document.createElement('p')
-  nte.innerHTML = content.replace("\n", "<br>")
+  nte.className = 'textview'
+  nte.innerHTML = dec.decode(content).replace("\n", "<br>")
   mainDiv.appendChild(nte)
 }
 
-function handleDrop (ev: Event, mainDiv: Element) {
+function handleDrop (ev: Event, htmlElements: HtmlElements) {
+  const mainDiv = htmlElements[mainDivId]
+  console.debug('is this the main div?', mainDiv)
   console.debug('drop event = ', ev)
   ev.preventDefault()
   const de = ev as DragEvent
@@ -63,20 +127,22 @@ function handleDrop (ev: Event, mainDiv: Element) {
     }
 
     const contents = ev.target.result
+    console.log("so turns out the contents is", contents);
 
-    if (typeof (contents) === 'string') {
+    if (typeof(contents) == "object") {
       if (!isImage) {
 	showText(mainDiv, contents)
       } else {
-        showImage(mainDiv, ev.target.result as string)
+        showImage(mainDiv, contents)
+	const uploadButton: HTMLButtonElement = htmlElements[uploadButtonId] as HTMLButtonElement
+	uploadButton.onclick = function(ev: Event) {
+	  console.debug("we clicked upload here", ev);
+	  console.debug(contents)
+	}
       }
     }
   }
-  if (isImage) {
-    fr.readAsDataURL(dt.files[0])
-  } else {
-    fr.readAsText(dt.files[0])
-  }
+  fr.readAsArrayBuffer(dt.files[0])
 }
 
 function handlePaste (ev: ClipboardEvent, mainDiv: Element) {
@@ -90,12 +156,22 @@ function handlePaste (ev: ClipboardEvent, mainDiv: Element) {
   }
 }
 
+type HtmlElements = {
+  [key: string]: Element
+};
+
 function initMain () {
   const mainDiv = document.querySelector(`#${mainDivId}`)
+  const uploadButton = document.querySelector(`#${uploadButtonId}`)
 
-  if (mainDiv == null) {
+  if (mainDiv == null || uploadButton == null) {
     console.log('init failed')
     return
+  }
+
+  const htmlElements: HtmlElements = {
+    [mainDivId]: mainDiv,
+    [uploadButtonId]: uploadButton,
   }
 
   window.addEventListener(Events.DRAG_OVER, function (ev: Event) {
@@ -103,7 +179,7 @@ function initMain () {
   }, false)
 
   window.addEventListener(Events.DROP, function (ev) {
-    handleDrop(ev, mainDiv)
+    handleDrop(ev, htmlElements)
   }, false)
 
   mainDiv.addEventListener(Events.DRAG_OVER, function (ev: Event) {
